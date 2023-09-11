2dc0056383ee1212342308288cf38d5f363800ce
fabiancord <fabianarielcordobes@gmail.com>
2023-09-11 16:31:46 -0300

    deploy back

--- api/src/db.js
+++ api/src/db.js
@@ -6,8 +6,8 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 console.log(DB_USER);
 
-let sequelize =
-	process.env.NODE_ENV === 'production'
+// let sequelize =
+// 	process.env.NODE_ENV === 'production'
 		// ? new Sequelize({
 		// 		database: DB_NAME,
 		// 		dialect: 'postgres',
@@ -35,10 +35,10 @@ let sequelize =
 		// 		native: false,
 		//   });
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
