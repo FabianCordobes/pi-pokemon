13ac3916a387a4db3a27d748df2fcfa7c1be71d0
fabiancord <fabianarielcordobes@gmail.com>
2023-09-11 16:35:18 -0300

    deploy back

--- api/src/db.js
+++ api/src/db.js
@@ -35,7 +35,7 @@ console.log(DB_USER);
 		// 		native: false,
 		//   });
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(`${DB_HOST}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
